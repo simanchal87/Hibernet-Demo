101d0e9cc07b4f5623965f7f55120bb04b15031d
sidash <sidash@3INDSIDASH1.ad.here.com>
2019-06-19 19:37:54 +0530

    Caching Level 1 - Creating Session 1 and 2

--- HibeDemo/src/main/java/com/simanchal/HibeDemo/App.java
+++ HibeDemo/src/main/java/com/simanchal/HibeDemo/App.java
@@ -14,34 +14,32 @@ public class App
 {
     public static void main( String[] args )
     {
-    	 	
+    	Student s1 = null;
     	    	
     	Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
     	ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
     	SessionFactory sf = con.buildSessionFactory(reg);
-    	Session session = sf.openSession();
     	
+    	//Session 1
+    	Session session = sf.openSession();
     	session.beginTransaction();
     	
-    	Student s1 =  (Student) session.get(Student.class, 3);
-    	
-    	System.out.println(s1.getSname());
-    	
-    	
-    	
-    	
-    	
-    /*	
-    	Collection<Laptop> lps = s1.getLaptop();
-    	
-    	for(Laptop l : lps) {
-    		
-    		System.out.println(l);
-    	}
+    	s1 =  (Student) session.get(Student.class, 3);
+    	System.out.println(s1);
     	
     	session.getTransaction().commit();
-    	*/
-    
+    	session.close();
+    	
+    	//Session 2
+    	Session session2 = sf.openSession();
+    	session2.beginTransaction();
+    	
+    	s1 =  (Student) session2.get(Student.class, 3);
+    	System.out.println(s1);
+    	
+    	session2.getTransaction().commit();
+    	session2.close();
+    	
     	
     }
 }
