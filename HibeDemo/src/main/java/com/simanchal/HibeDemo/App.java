db3943ac372425c054e7037dcd45ca44909bb04e
sidash <sidash@3INDSIDASH1.ad.here.com>
2019-06-21 13:02:17 +0530

    Hibernate Query Language part 3 - uses of SQL Query

--- HibeDemo/src/main/java/com/simanchal/HibeDemo/App.java
+++ HibeDemo/src/main/java/com/simanchal/HibeDemo/App.java
@@ -1,6 +1,9 @@
 package com.simanchal.HibeDemo;
 
+import java.util.List;
+
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -18,18 +21,18 @@ public class App
     	ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
     	SessionFactory sf = con.buildSessionFactory(reg);
     	
-    	double m = 60;
-    	
     	Session session = sf.openSession();
     	session.beginTransaction();
     	
-    	Query q = session.createQuery("select sum(mark) from Student where mark> :m");
-    	q.setParameter("m", m);
-    	Double mark = (Double) q.uniqueResult();
-    	
-    		System.out.println(mark);
-    	
-    	    	
+    	SQLQuery q = session.createSQLQuery("select * from Student where mark> 60");
+    	q.addEntity(Student.class);
+    	List <Student> students = q.list();
+    			
+    	for(Student s : students)
+    	{
+    		System.out.println(s);
+    	}
+    	   	
     	session.getTransaction().commit();
     }
 }
